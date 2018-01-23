bb2c5cea433efdb40dbfb2930c542d717e7a0fdf
Cagdas Yonder <cagdasyonder@gmail.com>
2018-01-23 00:44:45 -0500

    Fix

--- app/javascript/constants.js
+++ app/javascript/constants.js
@@ -6,5 +6,4 @@ export const ROOT_API_URL = "localhost:5000/api/v1";
 export const URL_REGEX = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/;
 
 export const CREATE_LINK = "create_link";
-export const FETCH_LINK = "fetch_link";
 export const FETCH_LINKS = "fetch_links";
