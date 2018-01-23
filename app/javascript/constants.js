eda623863d245405087def45f95890b0923b8810
Cagdas Yonder <cagdasyonder@gmail.com>
2018-01-23 11:53:47 -0500

    bug fix

--- app/javascript/constants.js
+++ app/javascript/constants.js
@@ -1,8 +1,8 @@
-export const ROOT_URL = "http://localhost:5000";
-export const ROOT_API_URL = "http://localhost:5000/api/v1";
+// export const ROOT_URL = "http://localhost:5000";
+// export const ROOT_API_URL = "http://localhost:5000/api/v1";
 
-// export const ROOT_URL = "http://cyonder.me";
-// export const ROOT_API_URL = "http://cyonder.me/api/v1";
+export const ROOT_URL = "http://cyonder.me";
+export const ROOT_API_URL = "http://cyonder.me/api/v1";
 
 // export const ROOT_URL = "https://url-shortener-on-react.herokuapp.com";
 // export const ROOT_API_URL = "https://url-shortener-on-react.herokuapp.com/api/v1";
